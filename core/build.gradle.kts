de05a0f3769a65b08aede321f4faf005dd8c28b0
flowerever <flowerever@rainbow.email>
2025-10-24 23:10:27 +0800

    Update

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -12,16 +12,17 @@ dependencies {
     implementation("org.apache.commons:commons-configuration2")
     implementation("commons-beanutils:commons-beanutils")
 
-    api("org.ow2.asm:asm-tree")
-    implementation("org.ow2.asm:asm")
-    implementation("org.ow2.asm:asm-analysis")
-    implementation("org.ow2.asm:asm-util")
-    implementation("org.ow2.asm:asm-commons")
+    val asmVersion = "9.7"
+    api("org.ow2.asm:asm-tree:$asmVersion")
+    implementation("org.ow2.asm:asm:$asmVersion")
+    implementation("org.ow2.asm:asm-analysis:$asmVersion")
+    implementation("org.ow2.asm:asm-util:$asmVersion")
+    implementation("org.ow2.asm:asm-commons:$asmVersion")
 
     implementation("com.github.leibnitz27:cfr") { isChanging = true }
-    implementation("org.vineflower:vineflower:1.10.1")
+    implementation("org.vineflower:vineflower:1.11.1")
     implementation("ch.qos.logback:logback-classic")
     implementation("software.coley:cafedude-core:2.1.1")
 
-//    externalLib("fernflower-15-05-20")
+	//externalLib("fernflower-15-05-20")
 }
--- core/src/main/java/me/nov/threadtear/CoreUtils.java
+++ core/src/main/java/me/nov/threadtear/CoreUtils.java
@@ -8,46 +8,96 @@ import java.lang.management.RuntimeMXBean;
 import java.util.List;
 import java.util.Objects;
 
-public class CoreUtils {
-  public static String getVersion() {
-    try {
-      return Objects.requireNonNull(CoreUtils.class.getPackage().getImplementationVersion());
-    } catch (NullPointerException e) {
-      return "(dev)";
+public class CoreUtils 
+{
+    public static String getVersion() 
+    {
+        try 
+        {
+            return Objects.requireNonNull(CoreUtils.class.getPackage().getImplementationVersion());
+        } 
+        catch (NullPointerException e) 
+        {
+            return "(dev)";
+        }
     }
-  }
 
-  public static boolean isNoverify() {
-    RuntimeMXBean runtimeMxBean = ManagementFactory.getRuntimeMXBean();
-    List<String> arguments = runtimeMxBean.getInputArguments();
-    return arguments.contains("-Xverify:none");
-  }
-
-  public static boolean isAttachable() {
-    try {
-      Class.forName("com.sun.tools.attach.VirtualMachine");
-      return true;
-    } catch (Exception e) {
-      return false;
+    public static boolean isNoverify() 
+    {
+        RuntimeMXBean rtMxBean = ManagementFactory.getRuntimeMXBean();
+        List<String> lstArguments = rtMxBean.getInputArguments();
+        return lstArguments.contains("-Xverify:none");
     }
-  }
 
-  public static int getClassSupport() {
-    VM vm = VM.constructVM(null);
-    int i = -1;
-    try {
-      for (i = 49; i < 60; i++) {
-        ClassNode cn = new ClassNode();
-        cn.version = i;
-        cn.name = String.valueOf(i);
-        cn.superName = "java/lang/Object";
-        vm.explicitlyPreload(cn);
-        if (vm.loadClass(cn.name) == null)
-          return i - 1;
-      }
-    } catch (Exception e) {
-      // ignore
+    public static boolean isAttachable() 
+    {
+        try 
+        {
+            Class.forName("com.sun.tools.attach.VirtualMachine");
+            return true;
+        } 
+        catch (Exception e) 
+        {
+            return false;
+        }
     }
-    return i - 1;
-  }
+
+    public static int getCurrentJavaVersion() 
+    {
+        String strVersion = System.getProperty("java.version");
+        if (strVersion.startsWith("1.")) 
+        {
+            return Integer.parseInt(strVersion.substring(2, 3));
+        } 
+        else 
+        {
+            int nDotIndex = strVersion.indexOf(".");
+            if (nDotIndex != -1) 
+            {
+                return Integer.parseInt(strVersion.substring(0, nDotIndex));
+            }
+            return Integer.parseInt(strVersion);
+        }
+    }
+
+    public static int getClassSupport() 
+    {
+        VM vmInstance = VM.constructVM(null);
+        int nCurrentJavaVersion = getCurrentJavaVersion();
+        int nMaxClassVersion = 44 + nCurrentJavaVersion;
+        
+        System.out.println("Current Java version: " + nCurrentJavaVersion);
+        System.out.println("Corresponding class file version: " + nMaxClassVersion);
+        
+        int nSupportedVersion = -1;
+        try 
+        {
+            for (int i = 49; i <= nMaxClassVersion + 5; i++) 
+            {
+                ClassNode clsNode = new ClassNode();
+                clsNode.version = i;
+                clsNode.name = "TestClass" + i;
+                clsNode.superName = "java/lang/Object";
+                
+                vmInstance.explicitlyPreload(clsNode);
+                if (vmInstance.loadClass(clsNode.name) == null) 
+                {
+                    System.out.println("VM does not support class version: " + i);
+                    nSupportedVersion = i - 1;
+                    break;
+                } 
+                else 
+                {
+                    nSupportedVersion = i;
+                    System.out.println("VM supports class version: " + i);
+                }
+            }
+        } 
+        catch (Exception e) 
+        {
+            System.err.println("Error during testing: " + e.getMessage());
+        }
+        
+        return nSupportedVersion;
+    }
 }
